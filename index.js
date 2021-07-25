0d842ff37e62dff29d29ca7afc2252a93852f6e7
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-25 01:59:54 -0300

    mini project for study in javascript Vanilla

---
+++ index.js
@@ -0,0 +1,39 @@
+const createCard = ({ title, href, img }) => `
+  <div class="card">
+    <img src="${img}" class="card-img-top"  alt="${img}" />
+
+    <div class="card ">
+      <h5>${title}</h5>
+      <a href="http://www.rifftrax.com/${href}" class="btn btn-primary"> Check it out </a>
+    </div>
+  </div>
+`;
+
+fetch("movies.json")
+  .then((response) => response.json())
+  .then((movies) => {
+    movies.sort((a, b) => a.title.localeCompare(b.title));
+
+    const cards = document.querySelector("#cards");
+    const elRow = document.createElement("div");
+    elRow.className = "row";
+    cards.appendChild(elRow);
+
+    const updateMovies = (movies) => {
+      elRow.innerHTML = movies
+        .map((card) => `<div class="col-6 col-lg-3">${card}</div>`)
+        .join("");
+    };
+
+    updateMovies(movies.map((movie) => createCard(movie)));
+
+    const search = document.querySelector(".search");
+    search.addEventListener("keyup", (evt) => {
+      const text = evt.target.value.toLowerCase();
+      updateMovies(
+        movies
+          .filter(({ title }) => title.toLowerCase().includes(text))
+          .map((movie) => createCard(movie))
+      );
+    });
+  });
